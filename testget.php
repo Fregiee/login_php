d321ccf268877313a69136eaa469dac621b5fe9b
Fregiee <167535403+Fregiee@users.noreply.github.com>
2024-05-16 20:45:04 +0800

    Add files via upload

---
+++ testget.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+
+if(!isset($_SESSION['username'])) {
+	header('Location: login.php');
+} 
+
+$order = $_POST['Orders'];
+$option = $order;
+$quantity = $_POST['quantity'];
+$cash = $_POST['cash'];
+$change = 0;
+$cost = 0;
+	if ( $option == "Fishball"){
+        $cost = 30 * $quantity;
+        $change = $cash - $cost;
+    }elseif($option == "Kikiam"){
+        $cost= 40 * $quantity;
+        $change = $cash - $cost;
+    }elseif($option == "Corndog"){
+        $cost = 50 * $quantity;
+        $change = $cash - $cost;
+    }
+echo "<h2> The total cost is " . $cost . "</h2>";
+echo "<h2> Your change is " . $change . "</h2>";
+echo "<h2>Thanks for your order! " . $_SESSION['username'] . "</h2>";
+?>
